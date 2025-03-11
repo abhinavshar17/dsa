0a053f4ff7c7c72ae29c20b6b77ca488d4c10b17
abhi75 <tinkuvyas06@gmail.com>
2025-03-11 22:31:29 +0530

    inbuilt

--- sorting/inbuilt.java
+++ sorting/inbuilt.java
@@ -1,10 +1,19 @@
 package sorting;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class inbuilt {
     public static void main(String[] args) {
-        int arr[]={90,55,78,12,45,50};
-        Arrays.sort(arr);
+        Integer arr[]={90,55,78,12,45,50};
+        int  p[]={90,55,78,12,45,50};
+         Arrays.sort(arr);
+         System.out.println(Arrays.toString(arr));
+
+        // Arrays.sort(arr,0,3);
+        // System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr,Collections.reverseOrder() );
         System.out.println(Arrays.toString(arr));
+
     }
 }
