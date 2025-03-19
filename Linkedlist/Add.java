073065df2eb925d698613b0329a1266b319810ab
abhi75 <tinkuvyas06@gmail.com>
2025-03-19 10:50:05 +0530

    remove,sizeaddmid

--- Linkedlist/Add.java
+++ Linkedlist/Add.java
@@ -1,72 +1,115 @@
-// package Linkedlist;
+package Linkedlist;
 
-// public class Add {
-// public static class Node{
-//     int data;
-//     Node next;
+public class Add {
+public static class Node{
+    int data;
+    Node next;
 
-//     public Node(int data){
-// this.data=data;
-// this.next=null;
-//     }
-// }
-// public static Node head;
-// public static Node tail;
+    public Node(int data){
+this.data=data;
+this.next=null;
+    }
+}
+public static Node head;
+public static Node tail;
+public static int size;
 
-// //method-->
-// public void addfirst(int data){
-//         // step1 -->create new node
-//         Node n1=new Node(data);
-//         if(head==null){
-//             head=tail=n1;
-//             return;
-//         }
-//         //step2--> n1 node ke next ko head ko point puarana head
-//          n1.next=head;  //link
+//method-->
+public void addfirst(int data){
+        // step1 -->create new node
+        Node n1=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=n1;
+            return;
+        }
+        //step2--> n1 node ke next ko head ko point puarana head
+         n1.next=head;  //link
+   
 
-//         //step3--> head ki value new node mien aa jae
-//         head=n1;
+        //step3--> head ki value new node mien aa jae
+        head=n1;
 
-//         //if ll is empty
+        //if ll is empty
         
        
-// }
+}
 
-// //add to last ke lae:
-// public void addlast(int data){
-//     Node n2=new Node(data);
-//     if(head==null){
-//         head=tail=n2;
-//         return;
-//     }
-//     tail.next=n2;
-//     tail=n2;
-// }
+//add to last ke lae:
+public void addlast(int data){
+    Node n2=new Node(data);
+    size++;
+    if(head==null){
+        head=tail=n2;
+        return;
+    }
+    tail.next=n2;
+    tail=n2;
+}
 
-// //printing:
-// public void Print(){
-//     if(head==null){
-//         System.out.println("ll is empty");
-//         return;
-//     }
-//     Node temp=head;
-//     while(temp!=null){
-//         System.out.print(temp.data +"->");
-//         temp=temp.next;
-//     }
-//     System.out.println("null");
-// }
+// add in any index
+public void addmid(int index,int data){
+    Node newNode=new Node(data);
+    Node temp=head;
+    int i=0;
+    while(i<index-1){
+        temp=temp.next;
+        i++;
+    }
+    newNode.next=temp.next;
+    temp.next=newNode;
+}
 
-//     public static void main(String[] args) {
-//         Add l1=new Add();
-//         l1.Print();
-//         l1.addfirst(2);
-//         l1.Print();
-//         l1.addfirst(1);
-//         l1.Print();
-//         l1.addlast(3);
-//         l1.Print();
-//         l1.addlast(4);
-//         l1.Print();
-//     }
-// }
+//remove:
+public int removef(){
+    if(size==0){ // size is zero 
+        System.out.println("ll is empty");
+        return Integer.MIN_VALUE;
+    }
+    else if(size==1){// head and tail at same place 
+        int val=head.data;
+        head=tail=null;
+        size=0;
+        return val;
+    }
+    int val=head.data;
+    head=head.next;
+    size--;
+    return val;
+}
+
+//printing:
+public void Print(){
+    if(head==null){
+        System.out.println("ll is empty");
+        return;
+    }
+    Node temp=head;
+    while(temp!=null){
+        System.out.print(temp.data +"->");
+        temp=temp.next;
+    }
+    System.out.println("null");
+}
+
+
+
+    public static void main(String[] args) {
+        Add l1=new Add();
+        l1.Print();
+        l1.addfirst(2);
+        l1.Print();
+        l1.addfirst(1);
+        l1.Print();
+        l1.addlast(3);
+        l1.Print();
+        l1.addlast(4);
+        l1.Print();
+        l1.addmid(1,9);
+        l1.Print();
+        System.out.println(l1.size);
+
+        l1.removef();
+        l1.Print();
+    }
+}
