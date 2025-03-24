932813b390e1b7a3ad498ac58d9cd5c51910258c
abhi75 <tinkuvyas06@gmail.com>
2025-03-24 19:19:55 +0530

    quicksort

--- sorting/bubble.java
+++ sorting/bubble.java
@@ -11,7 +11,7 @@ public class bubble {
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+                    arr[j+1]=temp;`
                 }
             }
         }
