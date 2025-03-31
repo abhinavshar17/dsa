a76520b1f20c441151b5277226482f0be92d8b42
abhi75 <tinkuvyas06@gmail.com>
2025-03-31 13:42:11 +0530

    revison for exams

--- Linkedlist/E1.java
+++ Linkedlist/E1.java
@@ -23,7 +23,14 @@ temp=temp.next;
 
     }
 }
-
+public static int length(Node head){
+    int count=0;
+    while(head!=null){
+        count++;
+        head=head.next;
+    }
+    return count;
+}
     public static class Node {
         int data;
         Node next;
@@ -38,17 +45,20 @@ temp=temp.next;
         Node b = new Node(3);
         Node c = new Node(9);
         Node d = new Node(10);
+        Node e = new Node(11);
 
         a.next = b;
         b.next = c;
         c.next = d;
+        d.next=e;
 
-        display(a);
-        System.out.println();
+        // display(a);
+        // System.out.println();
         
-        displayr(a);
-        System.out.println();
-        normaldis(a);
-        System.out.println("NULL"); // Optional: To indicate end of the list
+        // displayr(a);
+        // System.out.println();
+        // normaldis(a);
+        // System.out.println("NULL"); // Optional: To indicate end of the list
+    System.out.println(length(a));
     }
 }
---
+++ Linkedlist/E2.java
@@ -0,0 +1,83 @@
+package Linkedlist;
+
+public class E2 {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static class ll {
+        Node head = null;
+        Node tail = null;
+
+        void insertAtEnd(int data) {
+            Node temp = new Node(data);
+            if (head == null) {
+                head = tail = temp; // ✅ Fixed assignment
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+
+        void insertatfirst(int data) {
+            Node temp = new Node(data);
+            if (head == null) {
+                head = tail = temp;
+            } else {
+                temp.next = head;
+                head = temp;
+            }
+        }
+        void atindex(int index,int data){
+            
+            Node t=new Node(data);
+            Node temp = head;
+            for(int i=1;i<=index-1;i++){
+                    temp=temp.next;
+            }
+            t.next=temp.next;
+            temp.next=t;
+        }
+        Node lastn(Node head,int n){
+            Node slow=head;
+            Node fast=head;
+            for(int i=1;i<n;i++){
+                fast=fast.next;
+            }
+            while(fast!=null){
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
+        }
+
+        void display() { // ✅ Added display method
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("NULL");
+        }
+    }
+
+    public static void main(String[] args) {
+        ll myList = new ll(); // ✅ Fixed object creation
+
+        myList.insertAtEnd(4);
+        myList.insertAtEnd(5);
+        myList.display(); // 
+
+        myList.insertatfirst(3);
+        myList.display(); 
+        myList.atindex(1, 99);
+        myList.display();
+        Node result = myList.lastn(myList.head, 2);
+        System.out.println("2nd last node is: " + result.data);
+    }
+}
